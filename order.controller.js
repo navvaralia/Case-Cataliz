cbedecbbddfa4d1c3698b07c208ad549ab2c3260
navvaralia <124671417+navvaralia@users.noreply.github.com>
2023-02-07 16:56:17 +0700

    Add files via upload

---
+++ order.controller.js
@@ -0,0 +1,12 @@
+import {
+    postOrder,
+    // ...
+  } from 'use-cases/orders';
+  
+  import {
+    postPaymentLink
+  } from 'use-cases/payments';
+  
+  export default Object.freeze({
+    postOrder: (httpRequest) => postOrder(httpRequest, postPaymentLink),
+    // ...
